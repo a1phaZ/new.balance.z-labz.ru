5a025a2356d295c1794679710afb02741e5cef8f
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-09-11 13:46:11 +0500

    add day router

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/money-box', require('./moneybox'));
+router.use('/day', require('./day'));
 
 module.exports = router;
