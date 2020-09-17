2aed9c3a99d641adbf8777bac0903b5bfc015a19
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-09-17 11:32:20 +0500

    add popout

--- client/src/handlers/useApi.js
+++ client/src/handlers/useApi.js
@@ -1,8 +1,9 @@
-import {useState, useEffect, useCallback, useContext} from 'react';
+import React, {useState, useEffect, useCallback, useContext} from 'react';
 import queryString from 'query-string';
 import axios from 'axios';
-import {SET_ERROR, SET_SUCCESS_MESSAGE} from "../state/actions";
+import {SET_ERROR, SET_MODAL, SET_POPOUT, SET_SUCCESS_MESSAGE} from "../state/actions";
 import {State} from "../state";
+import {PopoutWrapper, ScreenSpinner} from "@vkontakte/vkui";
 
 export default url => {
 	const [isLoading, setLoading] = useState(false);
@@ -18,7 +19,8 @@ export default url => {
 		setResponse(null);
 		setError(null);
 		setLoading(true);
-	}, []);
+		dispatch({type: SET_POPOUT, payload: {popout: (<PopoutWrapper><ScreenSpinner /></PopoutWrapper>)}});
+	}, [dispatch]);
 
 	const {method = 'GET', params, ...bodyFields} = options;
 	const headers = {
@@ -38,16 +40,20 @@ export default url => {
 		if (!isLoading) return;
 
 		const fetchData = async () => {
+			setLoading(false);
 			return await axios(axiosOptions)
 				.then(async (response) => {
-					setLoading(false);
 					setResponse(response.data.data);
+					dispatch({type: SET_POPOUT, payload: {popout: null}});
+					dispatch({type: SET_MODAL, payload: {modal: null}});
 					dispatch({type: SET_SUCCESS_MESSAGE, payload: {message: response.data.message}});
 				})
 				.catch((error) => {
 					let e = error?.response ? error.response.data.error : new Error('Проблемы с подключением к API');
 					setError(e);
 					setLoading(false);
+					dispatch({type: SET_POPOUT, payload: {popout: null}});
+					dispatch({type: SET_MODAL, payload: {modal: null}});
 					dispatch({type: SET_ERROR, payload: {error: e}});
 				});
 		}
