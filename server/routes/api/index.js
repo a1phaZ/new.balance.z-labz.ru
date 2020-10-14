bcb0d39bb7ea2101688d8d78086217d94c339770
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-10-14 15:26:56 +0500

    add state route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/state', require('./state'));
 router.use('/money-box', require('./moneybox'));
 router.use('/item', require('./item'));
 router.use('/budget', require('./budget'));
