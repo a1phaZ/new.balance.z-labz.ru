a140241c932495422f5358fd49a2492e0d1d76a3
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-10-14 15:26:41 +0500

    add state route

---
+++ server/routes/api/state/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const MoneyBox = require('../../../models/moneybox');
+const Budget = require('../../../models/budget');
+const Item = require('../../../models/item');
+const toJson = require("../../../handlers/toJson");
+
+const findAccountsByUserId = async (userId) => {
+	return await MoneyBox.find({userId: userId})
+		.populate('operations');
+}
+
+const findBudgets = async (userId, date = new Date()) => {
+	return await Budget.find({userId: userId, year: new Date(date).getFullYear(), month: new Date(date).getMonth()})
+}
+
+const findItems = async (userId, title, year, month) => {
+	return await Item.find({userId: userId, tags: title.toLowerCase(), year: year, month: month});
+}
+
+async function budgetWithOutcomeF(array) {
+	let budgetWithOutcome = [];
+
+	for (const item of array) {
+		await findItems(item.userId, item.title, item.year, item.month)
+			.then(data => {
+				return data.map(el => el.sum).reduce((acc, cur) => acc + cur, 0);
+			})
+			.then(sum => {
+				item.sum = item.sum - sum;
+				return item;
+			})
+			.then(item => {
+				budgetWithOutcome = [...budgetWithOutcome, item];
+			});
+	}
+
+	return budgetWithOutcome;
+}
+
+router.get('/', async (req, res) => {
+	const {
+		query: { vk_user_id },
+		body: { date }
+	} = req;
+
+	req.accounts = await findAccountsByUserId(vk_user_id).then(data => data);
+	req.budgets = await findBudgets(vk_user_id, date).then(data => data);
+	req.budgets = await budgetWithOutcomeF(req.budgets);
+	await res.status(200).json(await toJson.dataToJson( {accounts: req.accounts, budgets: req.budgets}));
+	// await next();
+});
+
+module.exports = router;
