e9d6761a59b51b1a7e1d4b1207cc210df6fdc38f
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-10-16 15:05:35 +0500

    add budget list panel

--- client/src/panels/Budgets.js
+++ client/src/panels/Budgets.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {Panel, PanelHeader} from "@vkontakte/vkui";
+import {format} from 'date-fns';
+import ruLocale from 'date-fns/locale/ru';
+import {Footer, Group, Header, Panel, PanelHeader, PanelHeaderButton} from "@vkontakte/vkui";
 import ListOfItems from "../components/ListOfItems";
+import {SET_MODAL} from "../state/actions";
+import Icon28ListAddOutline from "@vkontakte/icons/dist/28/list_add_outline";
+import currency from "../handlers/currency";
 
-export default ({id, budgets, dispatch}) => {
+export default ({id, budgets, dispatch, date = new Date()}) => {
+	const budgetsStartSum = budgets.map(el => el.startSum).reduce((acc, cur) => acc + cur, 0);
+	const budgetsSum = budgets.map(el => el.sum).reduce((acc, cur) => acc + cur, 0);
 	return (
 		<Panel id={id}>
-			<PanelHeader>Budgets</PanelHeader>
-			<ListOfItems data={budgets} dispatch={dispatch} showAll={true} itemsName={'budgets'}/>
-
+			<PanelHeader
+				left={
+					<PanelHeaderButton onClick={() => dispatch({type: SET_MODAL, payload: {modal: 'add-budget'}})}>
+						<Icon28ListAddOutline/>
+					</PanelHeaderButton>
+				}
+			>
+				Бюджеты
+			</PanelHeader>
+			<Group
+				header={<Header mode="secondary">Ваши бюджеты
+					на {format(new Date(date), 'MMM yyyy', {locale: ruLocale})}</Header>}
+				separator="show"
+			>
+				<ListOfItems data={budgets} dispatch={dispatch} showAll={true} itemsName={'budgets'} needHide={false}/>
+				<Footer>Осталось {currency(budgetsSum)} из {currency(budgetsStartSum)}</Footer>
+			</Group>
 		</Panel>
 	)
 }
