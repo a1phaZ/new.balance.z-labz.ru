f31c255fc19bbcb420ce36979625f6d7dad3160a
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-10-27 11:14:13 +0500

    Updates

---
+++ precache-manifest.c8968388e7a448ef2ba9231c23903d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1a791178e264651f6d47345fb06d85",
+    "url": "/habit.z-labz.ru/index.html"
+  },
+  {
+    "revision": "61be26cd4d0b8867531a",
+    "url": "/habit.z-labz.ru/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "61be26cd4d0b8867531a",
+    "url": "/habit.z-labz.ru/static/js/2.c72c4be7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/habit.z-labz.ru/static/js/2.c72c4be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cbd0e876a69b4c4220",
+    "url": "/habit.z-labz.ru/static/js/main.03b7210c.chunk.js"
+  },
+  {
+    "revision": "5731f99fbec715f635a5",
+    "url": "/habit.z-labz.ru/static/js/runtime-main.4636e08c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.291bb446c0ce3d1d475110c96f76a0a1.js"
+  "/habit.z-labz.ru/precache-manifest.c8968388e7a448ef2ba9231c23903d0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/habit.z-labz.ru/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
