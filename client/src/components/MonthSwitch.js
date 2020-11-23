0378f81acd0930632f7d2b03d48cd1f745b3f141
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-11-23 14:21:13 +0500

    fix date range

--- client/src/components/MonthSwitch.js
+++ client/src/components/MonthSwitch.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import {addMonths, format, subMonths, isAfter} from 'date-fns';
+import {addMonths, format, subMonths, isAfter, isBefore} from 'date-fns';
 import {ru} from 'date-fns/locale';
 import Icon24BrowserBack from '@vkontakte/icons/dist/24/browser_back';
 import Icon24BrowserForward from '@vkontakte/icons/dist/24/browser_forward';
@@ -9,7 +9,8 @@ import {Button, Cell, Group} from "@vkontakte/vkui";
 export default ({onRefresh}) => {
 	const [state, dispatch] = useContext(State);
 	const dateFormat = 'LLLL yyyy';
-	const disabled = isAfter(addMonths(state.currentDate, 1), new Date());
+	const disabledAfter = isAfter(addMonths(state.currentDate, 1), new Date());
+	const disabledBefore = isBefore(new Date(state.currentDate), new Date(2015, 0, 1));
 	const [clickTime, setClickTime] = useState(0);
 	
 	const nextMonth = () => {
@@ -33,8 +34,8 @@ export default ({onRefresh}) => {
 	return (
 		<Group>
 			<Cell
-				before={<Button onClick={prevMonth} mode={'secondary'}><Icon24BrowserBack/></Button>}
-				asideContent={<Button disabled={disabled} onClick={nextMonth}
+				before={<Button disabled={disabledBefore} onClick={prevMonth} mode={'secondary'}><Icon24BrowserBack/></Button>}
+				asideContent={<Button disabled={disabledAfter} onClick={nextMonth}
 															mode={'secondary'}><Icon24BrowserForward/></Button>}
 				style={{textAlign: 'center', fontWeight: 'bold'}}
 			>
