5bfa2f13164bff94f4fea9cf0cbd3ad41f0c7cdd
Artemiy Zebzeev <pride.ots@gmail.com>
2020-12-04 22:45:35 +0500

    more

--- client/src/panels/More.js
+++ client/src/panels/More.js
@@ -1,36 +1,51 @@
 import React from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import {Cell, Group, Panel, PanelHeader} from "@vkontakte/vkui";
+import {Cell, Group, Header, Panel, PanelHeader} from "@vkontakte/vkui";
 import Icon28ShareExternalOutline from '@vkontakte/icons/dist/28/share_external_outline';
 import Icon28Users3Outline from '@vkontakte/icons/dist/28/users_3_outline';
 import Icon28AddCircleOutline from '@vkontakte/icons/dist/28/add_circle_outline';
 import Icon28ListCheckOutline from '@vkontakte/icons/dist/28/list_check_outline';
 
 export default ({id}) => {
+	const userId = new URL(window.location.href).searchParams.get('vk_user_id');
 	return (
 		<Panel id={id}>
 			<PanelHeader>Ещё</PanelHeader>
-			<Group>
+			<Group
+				header={
+					<Header
+						mode="secondary"
+					>
+						Дополнительно
+					</Header>
+				}
+			>
 				<Cell
 					expandable
 					before={
-						<Icon28ShareExternalOutline />
+						<Icon28ShareExternalOutline/>
 					}
-					onClick={() => {bridge.send("VKWebAppShare")}}
+					onClick={() => {
+						bridge.send("VKWebAppShare", {link: `https://vk.com/zlabz_balance#ref=${userId}`})
+					}}
 				>
 					Поделиться
 				</Cell>
 				<Cell
 					expandable
-					before={<Icon28Users3Outline />}
-					onClick={() => {bridge.send('VKWebAppJoinGroup', {group_id: 195358095})}}
+					before={<Icon28Users3Outline/>}
+					onClick={() => {
+						bridge.send('VKWebAppJoinGroup', {group_id: 195358095})
+					}}
 				>
 					Присоеденииться к Балансу
 				</Cell>
 				<Cell
 					expandable
-					before={<Icon28AddCircleOutline />}
-					onClick={() => {bridge.send('VKWebAppAddToHomeScreen')}}
+					before={<Icon28AddCircleOutline/>}
+					onClick={() => {
+						bridge.send('VKWebAppAddToHomeScreen')
+					}}
 				>
 					Добавить на главный экран
 				</Cell>
