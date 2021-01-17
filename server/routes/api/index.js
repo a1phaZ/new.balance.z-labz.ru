7d1f355c4ba0f5f1c2ba6793db310e400632020c
Artemiy Zebzeev <pride.ots@gmail.com>
2021-01-17 15:41:16 +0500

    add get user data functions

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const {getState} = require("./state");
-const {postUserInfo, getAllUserRawSessionData} = require('./user');
+const {
+    postUserInfo,
+    getAllUserRawSessionData,
+    getAllUserSessionDataFromTimestamp,
+    getNewUserFromTimestamp
+} = require('./user');
 const router = express.Router();
 
 router.get('/state', postUserInfo, getState);
 router.get('/user-session-data', getAllUserRawSessionData);
+router.get('/user-session-data-ts', getAllUserSessionDataFromTimestamp);
+router.get('/user-new-data', getNewUserFromTimestamp)
 router.use('/money-box', require('./moneybox'));
 router.use('/item', require('./item'));
 router.use('/budget', require('./budget'));
--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -1,5 +1,33 @@
 const User = require('../../../models/user');
+const endOfDay = require('date-fns/endOfDay');
+const startOfDay = require('date-fns/startOfDay');
+const endOfMonth = require('date-fns/endOfMonth');
+const startOfMonth = require('date-fns/startOfMonth');
 const {createError} = require('../../../handlers/error');
+
+/**
+ * Получение временного периода
+ * @param ts: Number - timestamp для получения периода
+ * @param opt {{ts: Boolean, month: Boolean, day: Boolean}}
+ * @returns {{end: Number | Date, begin: Number | Date}}
+ */
+const getDateRange = (ts, opt) => {
+    const beginDay = !opt.ts ? startOfDay(new Date(ts)) : startOfDay(+new Date(ts));
+    const endDay = !opt.ts ? endOfDay(new Date(ts)) : endOfDay(+new Date(ts));
+    const beginMonth = !opt.ts ? startOfMonth(new Date(ts)) : startOfMonth(+new Date(ts));
+    const endMonth = !opt.ts ? endOfMonth(new Date(ts)) : endOfMonth(+new Date(ts));
+
+    if (opt.month) {
+        return {
+            begin: beginMonth,
+            end: endMonth
+        }
+    }
+    return {
+        begin: beginDay,
+        end: endDay
+    }
+}
 
 /**
  * Функция записи данных о пользователе
@@ -61,7 +89,7 @@ const postUserInfo = async (req, res, next) => {
 }
 
 /**
- * Функция возвращает данные о пользователях
+ * Функция возвращает данные о всех пользователях
  * @param req
  * @param res
  * @param next
@@ -86,7 +114,59 @@ const getAllUserRawSessionData = async (req, res, next) => {
         );
 }
 
+/**
+ * Функция возвращает данные о пользователях за период времени
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void>}
+ */
+const getAllUserSessionDataFromTimestamp = async (req, res, next) => {
+    let {
+        query: {
+            ts = +new Date(),
+            day = false,
+            month = false
+        }
+    } = req;
+    const dateRange = getDateRange(+ts, {day: day, month: month, ts: true});
+    await User.find({'sessionData.timeStamp': {$gte: dateRange.begin, $lte: dateRange.end}})
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            console.log(err);
+            next(createError(400, 'Что-то пошло не так'));
+        })
+}
+
+/**
+ * Функция возвращает данные о новых пользователях за период
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void>}
+ */
+const getNewUserFromTimestamp = async (req, res, next) => {
+    let {
+        query: {
+            ts = +new Date(),
+            day = false,
+            month = false
+        }
+    } = req;
+    const dateRange = getDateRange(+ts, {day: day, month: month, ts: false});
+    await User.find({createdAt: {$gte: dateRange.begin, $lte: dateRange.end}})
+        .then(users => res.status(200).json(users))
+        .catch(err => {
+            console.log(err);
+            next(createError(400, 'Что-то пошло не так'));
+        })
+}
+
 module.exports = {
     postUserInfo,
-    getAllUserRawSessionData
+    getAllUserRawSessionData,
+    getAllUserSessionDataFromTimestamp,
+    getNewUserFromTimestamp
 }
