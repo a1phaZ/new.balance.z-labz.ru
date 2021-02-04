cd83e27e94ffc756629756cda56abb3874b16dac
Artemiy Zebzeev <pride.ots@gmail.com>
2021-02-04 11:55:59 +0500

    add shoplist router

--- server/models/shoplist.js
+++ server/models/shoplist.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
-const {format} = require('date-fns');
 mongoose.Promise = global.Promise;
 
 const ShopListSchema = new Schema({
-	userId: {type: String, required: [true, 'Отсутствует идентификатор пользователя']},
-	date: {type: String, default: format(new Date(), 'yyyy-MM-dd')},
 	list: [{
-		id: {type: Number},
 		title: {type: String, required: true},
-		done: {type: Boolean, default: false}
 	}]
-});
+}, {timestamps: true});
 
 const ShopList = model('ShopList', ShopListSchema);
 
--- server/routes/api/shoplist/index.js
+++ server/routes/api/shoplist/index.js
@@ -1,19 +1,85 @@
 const express = require('express');
 const router = express.Router();
-// const mongoose = require('mongoose');
-const ShopList = require('../../../models/shoplist');
-// const {getState} = require("../state");
+const mongoose = require('mongoose');
+const { Types: {ObjectId}} = mongoose;
 
-router.patch('/', async (req, res, next) => {
+const {createError, getMongooseError, setErrorStatusCodeAndMessage} = require('../../../handlers/error');
+const ShopList = require('../../../models/shoplist');
+const toJson = require("../../../handlers/toJson");
+
+router.get('/:id', async (req, res, next) => {
 	const {
-		body: { list },
-		query: { vk_user_id }
+		params: {
+			id
+		}
 	} = req;
-	const date = new Date().setHours(0,0,0,0);
-	const nextDate = new Date().setHours(24, 0, 0, 0);
-	const filter = { userId: vk_user_id, date: {$gte: date, $lte: nextDate}}
-	await ShopList.findOneAndUpdate(filter, {$set: { list: list }}, {new: true, upsert: true})
-		.then(doc => res.json(doc));
-}/*, getState*/);
+	
+	if (!(ObjectId.isValid(id) && (new ObjectId(id)).toString() === id)) {
+		return next(createError(400,  'Ошибка идентификатора объекта'));
+	}
+	
+	await ShopList.findById(id)
+		.then(({list}) => {
+			return toJson.dataToJson({list})
+		})
+		.then(data => res.status(200).json(data))
+		.catch(err => {
+			if (err.errors) {
+				return next(createError(400, getMongooseError(err)))
+			}
+			if (err.reason) {
+				const e = setErrorStatusCodeAndMessage(err);
+				next(createError(e.statusCode, e.message));
+			}
+			return next(createError(err.statusCode, err.message))
+		});
+});
+
+router.post('/add', async (req, res, next) => {
+	const {
+		body: {
+			list
+		}
+	} = req;
+	
+	if (!list) {
+		return next(createError(400, 'Отсутствуют обязательные параметры'));
+	}
+	
+	if (list.length === 0) {
+		return next(createError(400, 'Список не должен быть пустым'));
+	}
+	
+	const titleOnly = list.reduce((acc, {done, title}) => {
+		if (!done && title) {
+			return [...acc, {title}];
+		}
+		return acc;
+	}, []);
+	
+	if (titleOnly.length === 0) {
+		return next(createError(400, 'Не найдено ни одного названия в списке'));
+	}
+	
+	const listToSave = new ShopList({
+		list: titleOnly
+	});
+	
+	await listToSave.save()
+		.then(({_id}) => {
+			return toJson.dataToJson({id: _id})
+		})
+		.then(data => res.status(200).json(data))
+		.catch(err => {
+			if (err.errors) {
+				return next(createError(400, getMongooseError(err)))
+			}
+			if (err.reason) {
+				const e = setErrorStatusCodeAndMessage(err);
+				next(createError(e.statusCode, e.message));
+			}
+			return next(createError(err.statusCode, err.message))
+		});
+});
 
 module.exports = router;
