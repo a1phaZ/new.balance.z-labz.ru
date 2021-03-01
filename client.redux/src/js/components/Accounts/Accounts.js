c6eade596a194b14add2a6d9d60953779a07490c
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-03-01 15:25:59 +0500

    add accounts

---
+++ client.redux/src/js/components/Accounts/Accounts.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from "redux";
+import {getData, setId} from "../../store/api/actions";
+import {connect} from "react-redux";
+import ItemsList from "../ItemsList";
+import {Div, Group, Header, PanelSpinner, Title} from "@vkontakte/vkui";
+import currency from "../../services/currency";
+import {setStory} from "../../store/router/actions";
+
+class Accounts extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.callApi = this.callApi.bind(this);
+		this.sumOfAll = this.sumOfAll.bind(this);
+		this.setAccount = this.setAccount.bind(this);
+	}
+	
+	componentDidMount() {
+		this.callApi('/accounts');
+	}
+	
+	callApi = (url) => {
+		this.props.getData(url);
+	}
+	
+	setAccount = ({type, id}) => {
+		this.props.setId({[type]: id});
+		this.props.setStory('accounts', 'info');
+	}
+	
+	sumOfAll = (accounts) => {
+		return accounts.map(el => el.sum).reduce((acc, cur) => acc + cur, 0);
+	}
+	
+	render() {
+		const {accounts, isLoading} = this.props;
+		return (
+			<Group
+				header={<Header mode="secondary">Ваши счета</Header>}
+				separator="show"
+			>
+				<Div>
+					{isLoading && <PanelSpinner/>}
+					{!isLoading &&
+						<>
+							<Title level="1" weight="semibold" style={{marginBottom: 16}}>{currency(this.sumOfAll(accounts))}</Title>
+							<ItemsList data={accounts} setAccount={this.setAccount}/>
+						</>
+					}
+				</Div>
+			</Group>
+		)
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		accounts: state.api.accounts,
+		error: state.api.error,
+		isLoading: state.api.isLoading.accounts
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		dispatch,
+		...bindActionCreators({getData, setId, setStory}, dispatch)
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Accounts);
---
+++ client.redux/src/js/components/Accounts/accountRichCell.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Caption, RichCell} from "@vkontakte/vkui";
+import {format} from "date-fns";
+import ruLocale from "date-fns/locale/ru";
+import currency from "../../services/currency";
+import Color from "../Color";
+
+export default function accountRichCell({tagsShow = true}) {
+	return (item, index) => {
+		if (item.caption) {
+			return (
+				<Caption level="2" weight="semibold" caps
+								 key={index}>
+					{format(new Date(item.date), 'dd MMMM yyyy ', {locale: ruLocale})}
+					<br/>
+					{item.income && item.outcome ? ' [ ' : null}
+					<Color value={item.income} color={'green'}/>
+					{item.income && item.outcome ? ' | ' : null}
+					<Color value={item.outcome} color={'red'}/>
+					{item.income && item.outcome ? ' ]' : null}
+				</Caption>
+			)
+		}
+		
+		const tags = item.tags.filter(tag => !!tag.length).map(
+			(tag, index) =>
+				<span key={index} style={{
+					marginLeft: '5px',
+					marginRight: '5px',
+					marginBottom: '5px',
+					maxWidth: '100%',
+					overflow: 'hidden',
+					overflowWrap: 'normal',
+					// background: 'var(--counter_secondary_background)',
+					// color: 'var(--counter_secondary_text)',
+					// padding: '5px'
+				}}><b>{tag}</b></span>
+		);
+		let caption;
+		let after;
+		if (tagsShow) {
+			caption = item.tags.length !== 0 && <div style={{display: 'flex', flexWrap: 'wrap'}}><span>Теги:</span> {tags}</div>
+			after = item.income ? currency(item.sum) : currency(-1 * item.sum)
+		} else {
+			caption = <div style={{display: 'flex', flexWrap: 'wrap'}}>{format(new Date(item.date), 'dd MMMM yyyy ', {locale: ruLocale})}</div>
+			after = item.income ? currency(item.sum) : currency(-1 * item.sum);
+		}
+		return (
+			<RichCell
+				key={index}
+				multiline
+				caption={caption}
+				after={after}
+				data-id={item._id}
+				onClick={() => {
+					console.log('Вызываем модалку для редактирования')
+				}}
+			>
+				{item.title}
+			</RichCell>
+		)
+	};
+}
