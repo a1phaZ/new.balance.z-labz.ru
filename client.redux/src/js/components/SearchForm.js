f04bd8a3785267f6a451a855843ff0b2a56c7c31
Artemiy Zebzeev <pride.ots@gmail.com>
2021-03-06 13:03:02 +0500

    add SearchForm

---
+++ client.redux/src/js/components/SearchForm.js
@@ -0,0 +1,31 @@
+import React, {useRef, useState, useEffect} from 'react';
+import {FormLayout, Search} from "@vkontakte/vkui";
+
+export default ({onSearch, initialString = ''}) => {
+
+	const [searchStr, setSearchStr] = useState(() => initialString);
+	const searchRef = useRef(null);
+
+	useEffect(() => {
+		onSearch(searchStr);
+	}, [searchStr, onSearch])
+
+	return (
+		<>
+		<FormLayout
+			onSubmit={(e) => {
+				e.preventDefault();
+				searchRef.current.focus();
+			}}
+		>
+			<Search
+				value={searchStr}
+				onChange={(e) => {
+					setSearchStr(e.currentTarget.value.substring(0, 20))
+				}}
+			/>
+		</FormLayout>
+		<input type={'radio'} style={{position: 'absolute', height: '0', width: '0', top: '0', left: '0'}} ref={searchRef}/>
+		</>
+	)
+}
